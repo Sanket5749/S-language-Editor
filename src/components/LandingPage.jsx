0fe47ec644abe40a607bdae12d93cd862871eb1c
Sanket Patil <sanketpatil574918@gmail.com>
2025-08-09 15:24:38 +0530

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -20,7 +20,7 @@ export default function LandingPage() {
       {!showSplash && (
         <div className="min-h-screen bg-gradient-to-br justify-center from-gray-900 via-gray-800 to-gray-900 flex flex-col">
           <header className="w-full py-8 flex flex-col items-center">
-            <h1 className="text-white">ARCLANG Editor</h1>
+            <h1 className="text-white">SLANG Editor</h1>
             <p className="text-lg md:text-xl text-gray-300 mb-4 text-center max-w-xl">
               Learn, practice, and master programming in SLANG with
               interactive challenges and a modern online IDE.
@@ -31,3 +31,4 @@ export default function LandingPage() {
     </div>
   );
 }
+
